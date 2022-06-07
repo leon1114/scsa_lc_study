7531f5af567a64ac404e6b3ca94bd9eb5666f50a
CheongMyungJ <101340453+CheongMyungJ@users.noreply.github.com>
2022-06-08 01:12:56 +0900

    Update AddTwoNumbers.cpp

--- 2022/06/07/CheonMyungJ/AddTwoNumbers.cpp
+++ 2022/06/07/CheonMyungJ/AddTwoNumbers.cpp
@@ -2,36 +2,34 @@
 
 class Solution {
 public:
-	ListNode node_arr[110];
-	int node_cnt = 0;
 	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		node_cnt = 0;
 		ListNode* ret_node = nullptr;
 
 		int up_num = 0;
 		ListNode* current_node = nullptr;
 		do {
 			if (current_node != nullptr) {
-				current_node->next = &node_arr[node_cnt++];
+				current_node->next = new ListNode();
 				current_node = current_node->next;
 			}
 			else {
-				ret_node = current_node = &node_arr[node_cnt++];
+				ret_node = current_node = new ListNode();
 			}
 
 			current_node->val += up_num;
 			current_node->val += (l1 == nullptr) ? 0 : l1->val;
 			current_node->val += (l2 == nullptr) ? 0 : l2->val;
 			up_num = current_node->val / 10;
-			current_node->val %= 10;
+	
+};
+		current_node->val %= 10;
 			l1 = l1 == nullptr? nullptr : l1->next;
 			l2 = l2 == nullptr? nullptr : l2->next;
 		} while (l1 != nullptr || l2 != nullptr);
 		if (up_num == 1) {
-			current_node->next = &node_arr[node_cnt++];
+			current_node->next = new ListNode();
 			current_node = current_node->next;
 			current_node->val = 1;
 		}
 		return ret_node;
 	}
-};
