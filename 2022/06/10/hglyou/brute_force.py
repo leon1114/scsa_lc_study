582f51bfeeddfe684aac90026ecb4710b8189708
hglyou <hglyou@toss.im>
2022-06-09 09:13:30 +0900

    added 0610 bruteforce solution

---
+++ 2022/06/10/hglyou/brute_force.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        """
+        Bruteforce solution + early stops 
+        
+        어차피 s 의 length 가 max 1000 이므로, n^2 solution 으로 풀어도 상관 없음. 
+
+        Runtime: 2650 ms, faster than 24.14% of Python3 online submissions for Longest Palindromic Substring.
+        Memory Usage: 13.8 MB, less than 90.97% of Python3 online submissions for Longest Palindromic Substring.
+        """
+        
+        max_len = 1
+        max_str = s[0]
+        
+        for start in range(len(s)):
+            if len(s) - start <= max_len:
+                break
+            for end in range(len(s), start, -1):
+                if end - start <= max_len:
+                    break
+                if s[start] != s[end-1]:
+                    continue
+                substr = s[start: end]
+                if substr == substr[::-1]:
+                    if end - start > max_len:
+                        max_len = end - start
+                        max_str = substr
+                        
+        return max_str
+        
+        
+        
