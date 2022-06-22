1f6375f01956ae0602c6e2d9e5d8f15bf277f095
HanchanHwnag <lintz75@gmail.com>
2022-06-22 09:44:05 +0900

    Update hhc.c

--- 2022/06/22_remove_nth_node_from_end_of_list/hhc.c
+++ 2022/06/22_remove_nth_node_from_end_of_list/hhc.c
@@ -15,23 +15,24 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     int node_move_cnt = 0;
     Node *ptr = head, *ptr_before_remove = head, *ptr_remove = head;
     
+    // ListNode 끝까지 돌려봄.
     while(ptr != NULL) {
         node_move_cnt++;
-        if (node_move_cnt > n) {
+        if (node_move_cnt > n) { // 돌다가 지워야할 놈 위치를 저장해놓음
             ptr_remove = ptr_remove->next;
         }
-        if (node_move_cnt > n + 1) {
+        if (node_move_cnt > n + 1) {  // 돌다가 지워야할 놈 바로 이전놈 위치를 저장해놓음
             ptr_before_remove = ptr_before_remove->next;
         }
         ptr = ptr->next;
     }
     
     // ptr point the node to remove
-    if (ptr_remove != ptr_before_remove) {
+    if (ptr_remove != ptr_before_remove) { // 지워야할놈 != 지워야할놈 바로전놈이면 지워야할놈 지움
         ptr = ptr_before_remove->next;
         ptr_before_remove->next = ptr->next;
         free(ptr);
-    } else {
+    } else { // 지워야할놈 == 지워야할놈 바로전놈이면 => 지워야할놈이 head라는 뜻. 헤드를 한칸 뒤로 옮기고 리턴
         head = head->next;
     }
     return head;
