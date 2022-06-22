521800f49e6461d439da93316ac958d1b5d4afa8
HanchanHwnag <lintz75@gmail.com>
2022-06-22 22:57:34 +0900

    Update hhc.cpp

--- 2022/06/23_generate_parentheses/hhc.cpp
+++ 2022/06/23_generate_parentheses/hhc.cpp
@@ -7,39 +7,35 @@ using std::endl;
 using std::string;
 using std::vector;
 
-// Runtime: 39 ms, faster than 5.91% of C++ online submissions for Generate Parentheses.
-// Memory Usage: 25.3 MB, less than 5.56% of C++ online submissions for Generate Parentheses.
+// 뭐지 왤케 느리지..? 어케 빨리하누 string += char; 요게 느린덧?
+// Runtime: 16 ms, faster than 12.94% of C++ online submissions for Generate Parentheses.
+// Memory Usage: 15.8 MB, less than 19.88% of C++ online submissions for Generate Parentheses.
 class Solution {
 public:
-    vector<string> hanoi(string s, int open_stack, int n, int closed) {
-        vector<string> ret;
-        vector<string> tmp;
+    vector<string> sol;
+    void hanoi(string s, int open_stack, int n, int closed) {
         string s_tmp;
-        
         if (!open_stack && closed == n) {
-            ret.insert(ret.begin(), s);
-            return ret;
+            sol.insert(sol.end(), s);
         }
         
         // open_stack < n, Append '(', then dfs
         if (open_stack + closed < n) {
             s_tmp = s;
             s_tmp += '(';
-            tmp = hanoi(s_tmp, open_stack + 1, n, closed);
-            ret.insert(ret.end(), tmp.begin(), tmp.end());
+            hanoi(s_tmp, open_stack + 1, n, closed);
         }
 
         // open_stack > 0, Append ')', then dfs
         if (open_stack > 0) {
             s_tmp = s;
             s_tmp += ')';
-            tmp = hanoi(s_tmp, open_stack - 1, n, closed + 1);
-            ret.insert(ret.end(), tmp.begin(), tmp.end());
+            hanoi(s_tmp, open_stack - 1, n, closed + 1);
         }
-        return ret;
     }
     vector<string> generateParenthesis(int n) {
-        return hanoi("", 0, n, 0);
+        hanoi("", 0, n, 0);
+        return sol;
     }
 };
 
