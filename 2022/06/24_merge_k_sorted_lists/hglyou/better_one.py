9be53beaaaacce9fff10da90552f7eadfef74a67
hglyou <hglyou@toss.im>
2022-06-23 10:52:50 +0900

    added time complexity analysis to 0624 solution

--- 2022/06/24_merge_k_sorted_lists/hglyou/better_one.py
+++ 2022/06/24_merge_k_sorted_lists/hglyou/better_one.py
@@ -18,6 +18,16 @@ class Solution:
         이 때 예외케이스는 디큐시킨 리스트에 더 이상 넣을 숫자가 없는 경우. 
         이 때는 삽입과정을 건너뛰고 정렬된 리스트에서 앞에있던 숫자를 정답 리스트에 넣기만 하면 됨. 
 
+        시간복잡도: 
+
+        첫 값 리스트 생성: O(k)
+        첫 값 리스트 정렬: O(klogk)
+        정답 생성:
+            최대 숫자갯수 만큼 iter 하면서 -> 최대 10000번 (우연히 k 와 max 값이 동일하므로 그냥 k 로 봐도 됨.)
+            최대 리스트 갯수만큼 원소를 가지고 있는 첫 값 리스트 대상으로 바이너리 서치 한 번씩 이므로, -> O(logk)
+
+        --> 시간 복잡도 O(klogk)
+        
         Runtime: 106 ms, faster than 92.77% of Python3 online submissions for Merge k Sorted Lists.
         Memory Usage: 18.7 MB, less than 11.97% of Python3 online submissions for Merge k Sorted Lists.
         """
