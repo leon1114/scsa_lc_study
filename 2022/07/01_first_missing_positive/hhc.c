c6abfdedce881a7dd39b82c851a56f7ad9b2a675
HanchanHwnag <lintz75@gmail.com>
2022-06-30 22:03:11 +0900

    Create hhc.c

---
+++ 2022/07/01_first_missing_positive/hhc.c
@@ -0,0 +1,16 @@
+// 이거도 O(n)으로 쳐주나?
+// Runtime: 174 ms, faster than 63.87% of C online submissions for First Missing Positive.
+// Memory Usage: 29.9 MB, less than 93.43% of C online submissions for First Missing Positive.
+
+int firstMissingPositive(int* nums, int numsSize){
+    int ret = 1;
+    int map[500005] = {0, };
+    for(int i = 0; i < numsSize; i++) {
+        if (nums[i] == ret) {
+            while(map[++ret]);
+        } else if (nums[i] <= 50000 && nums[i] > 0) {
+            map[nums[i]] = 1;
+        }
+    }
+    return ret;
+}
