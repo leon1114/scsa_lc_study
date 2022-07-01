307c24745c3e0b92a072deaca6f4d0eafd9cfd22
HanchanHwnag <lintz75@gmail.com>
2022-07-01 21:56:23 +0900

    Update hhc.c

--- 2022/07/01_first_missing_positive/hhc.c
+++ 2022/07/01_first_missing_positive/hhc.c
@@ -8,7 +8,7 @@ int firstMissingPositive(int* nums, int numsSize){
     for(int i = 0; i < numsSize; i++) {
         if (nums[i] == ret) {
             while(map[++ret]);
-        } else if (nums[i] <= 500000 && nums[i] > 0) {
+        } else if (nums[i] <= 500000 && nums[i] > 0) { // 50000으로 돌려도 accept ㅋㅋㅋ 고침
             map[nums[i]] = 1;
         }
     }
