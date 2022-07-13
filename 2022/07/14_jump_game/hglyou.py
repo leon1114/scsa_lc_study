abf3cbf0f26b3996932404031bbc39bf2159e48f
Hyung Gyun, Lyou <leon1114@naver.com>
2022-07-14 08:37:53 +0900

    Update hglyou.py

--- 2022/07/14_jump_game/hglyou.py
+++ 2022/07/14_jump_game/hglyou.py
@@ -1,8 +1,11 @@
 class Solution:
     """
-    O(n)
     Runtime: 639 ms, faster than 67.29% of Python3 online submissions for Jump Game.
     Memory Usage: 15.3 MB, less than 18.22% of Python3 online submissions for Jump Game.
+    
+    nums list 를 뒤에서 부터 순회하면서 destination 에 도달할 수 있는지 여부를 파악, 도달 가능하다면 현재 위치를 새로운 destination 으로 update 한다. 
+    어떻게 도달했냐는 중요하지 않고, 도달할 수 있냐 없냐만 판단하면 되므로 일단 최종 목적지에 도달할 수 있는 위치 하나만 찾으면 그 위치가 새로운 목적지가 되는 것임. 
+    요것을 반복했을때, destination 이 0 이 되어있다면 0 에서 최종 목적지까지 도달할 수 있다는 뜻. 
     """
     def canJump(self, nums: List[int]) -> bool:
         destination = len(nums) - 1
