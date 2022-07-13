514c61498239898014459a6c925c8e658ac883b0
Hyung Gyun, Lyou <leon1114@naver.com>
2022-07-14 08:34:02 +0900

    Update hglyou.py

--- 2022/07/14_jump_game/hglyou.py
+++ 2022/07/14_jump_game/hglyou.py
@@ -1,10 +1,24 @@
 class Solution:
     """
+    Runtime: 639 ms, faster than 67.29% of Python3 online submissions for Jump Game.
+    Memory Usage: 15.3 MB, less than 18.22% of Python3 online submissions for Jump Game.
+    """
+    def canJump(self, nums: List[int]) -> bool:
+        destination = len(nums) - 1
+        
+        for i in reversed(range(len(nums) - 1)):
+            if destination - i <= nums[i]:
+                destination = i
+        
+        return destination == 0
+
+
+    """
     O(n^2)
     Runtime: 3289 ms, faster than 10.24% of Python3 online submissions for Jump Game.
     Memory Usage: 15.1 MB, less than 82.21% of Python3 online submissions for Jump Game.
     """
-    def canJump(self, nums: List[int]) -> bool:
+    def canJumpOn2(self, nums: List[int]) -> bool:
         dp = [False for _ in range(len(nums))]
         dp[0] = True
 
