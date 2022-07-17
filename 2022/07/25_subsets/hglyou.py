acbae629ca70735a735b901ab60048369569e9f9
Hyung Gyun, Lyou <leon1114@naver.com>
2022-07-17 22:46:07 +0900

    Update hglyou.py

--- 2022/07/25_subsets/hglyou.py
+++ 2022/07/25_subsets/hglyou.py
@@ -1,5 +1,8 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        """
+        각 숫자를 선택한 경우, 선택하지 않은경우로 나눠서 백트랙킹
+        """
         answer = []
         
         def bt(idx, tmp_result):
