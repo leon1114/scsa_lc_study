ab359cd946a3b90a247754fd54e0d08aa348c789
Hyung Gyun, Lyou <leon1114@naver.com>
2022-07-19 09:07:00 +0900

    Update hglyou.py

--- 2022/07/26_word_search/hglyou.py
+++ 2022/07/26_word_search/hglyou.py
@@ -1,11 +1,31 @@
-from collections import deque
+from collections import deque, defaultdict
 
 class Solution:
     """
-    Runtime: 6746 ms, faster than 63.51% of Python3 online submissions for Word Search.
-    Memory Usage: 13.9 MB, less than 50.90% of Python3 online submissions for Word Search.
+    Runtime: 5603 ms, faster than 79.46% of Python3 online submissions for Word Search.
+    Memory Usage: 13.8 MB, less than 92.85% of Python3 online submissions for Word Search.
     """
+    
     def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        # 조금이라도 pruning 해서 빠르게해보려는 발버둥 1 
+        if len(word) > len(board) * len(board[0]):
+            return False
+        
+        # 조금이라도 pruning 해서 빠르게해보려는 발버둥 2
+        word_cnt = defaultdict(int)
+        board_word_cnt = defaultdict(int)
+        for c in word:
+            word_cnt[c] += 1
+        
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                board_word_cnt[board[r][c]] += 1
+        
+        for c, cnt in word_cnt.items():
+            if board_word_cnt[c] < cnt:
+                return False
+        
         directions = [(1,0), (0, -1), (-1, 0), (0, 1)]
         def dfs(s_r, s_c, target_idx, visited):
             if target_idx == len(word) - 1 and board[s_r][s_c] == word[target_idx]:
@@ -28,7 +48,7 @@ class Solution:
                 
                 if result:
                     return True
-            
+
             return False 
             
         for r in range(len(board)):
