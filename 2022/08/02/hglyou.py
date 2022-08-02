7981c13b9529331f7cc65c984a4bae7cd9605395
Hyung Gyun, Lyou <leon1114@naver.com>
2022-08-02 17:25:38 +0900

    Update hglyou.py

--- 2022/08/02/hglyou.py
+++ 2022/08/02/hglyou.py
@@ -1,26 +1,46 @@
-from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
+        if root is None:
             return []
-
-        q = deque()
-        q.append((root, 0))
-        ans = []
-
-        while q:
-            cur, level = q.popleft()
-            if len(ans) <= level:
-                ans.append([])
-            ans[level].append(cur.val if cur else None)
-
-            if cur.left:
-                q.append((cur.left, level + 1))
-            if cur.right:
-                q.append((cur.right, level + 1))
         
-        for i in range(len(ans)):
-            if i % 2:
-                ans[i] = ans[i][::-1]
+        queues = [[], []]
+        turn = 0
+        queues[0].append((root, 0))
+        ans = []
+        
+        while True:
+            if turn % 2 == 0:
+                cur, child = queues
+            else:
+                cur, child = queues[::-1]
+            
+            if len(cur) == 0:
+                break
+                
+            while cur:
+                node, level = cur.pop()
+                if len(ans) <= level:
+                    ans.append([])
+                
+                ans[level].append(node.val)
+                
+                children = [node.left, node.right] if turn % 2 == 0 else [node.right, node.left]
+                
+                for c in children:
+                    if c:
+                        child.append((c, level+1))
+            
+            turn += 1
         
         return ans
+        
+        
+        
+        
+        
