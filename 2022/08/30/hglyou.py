b6c3eefca879eabf82d5312549fb78779c68a93c
Hyung Gyun, Lyou <leon1114@naver.com>
2022-08-30 08:23:52 +0900

    Update hglyou.py

--- 2022/08/30/hglyou.py
+++ 2022/08/30/hglyou.py
@@ -1,6 +1,6 @@
 """
-Runtime: 66 ms, faster than 61.87% of Python3 online submissions for Find Peak Element.
-Memory Usage: 14.1 MB, less than 44.04% of Python3 online submissions for Find Peak Element.
+Runtime: 58 ms, faster than 75.80% of Python3 online submissions for Find Peak Element.
+Memory Usage: 14 MB, less than 82.49% of Python3 online submissions for Find Peak Element.
 """
 class Solution:
     """
@@ -12,22 +12,13 @@ class Solution:
         nums = [float('-inf')] + nums + [float('-inf')]
         l, r = 1, len(nums) - 2
         
-        """
-        항상 헷갈리는 바이너리 서치의 종료 조건과 cursor 이동에 대한 내용 
-        - 가장 마지막 상황을 가정하고 조건들을 정해나가면 된다. 
-        - 가장 마지막 상황 : inf, 1, 1, 이 남은 경우. l, r = 1, 2
-            - m = 1
-            - nums[1] >= nums[2] 임 
-            - r 은 2 에서 1이 됨. 
-            - 이 때 l <= r 을 종료조건으로 사용하면 무한루프 
-            - l < r 로 루프 끝내고 l 을 정답으로 사용하면 됨. 
-        """
-        while l < r:
+        while l <= r:
             m = (l + r) // 2
-            if nums[m] >= nums[m+1]:
-                r = m
-            else:
+            if nums[m] < nums[m+1]:
                 l = m + 1
-        
-        return l - 1
-        
+            elif nums[m-1] > nums[m]:
+                r = m - 1
+            else:
+                # -inf 를 더해준 것 보정하여 return 
+                return m - 1
+                
