9078fa612b608fc20896d4f2a6755a50ca1ad313
CheongMyungJ <101340453+CheongMyungJ@users.noreply.github.com>
2022-09-19 23:49:17 +0900

    Update CheongMyungJ.cpp

--- 2022/09/19/CheongMyungJ.cpp
+++ 2022/09/19/CheongMyungJ.cpp
@@ -1,6 +1,6 @@
 //Runtime: 72 ms, faster than 99.89% of C++ online submissions for Kth Largest Element in an Array.
 //Memory Usage: 45.4 MB, less than 87.30% of C++ online submissions for Kth Largest Element in an Array.
-//배열은 10만개지만 숫자범위는 2만개라서 O(n + m)으로 처리함 여기서 n은 최대 10만, m은 2
+//배열은 10만개지만 숫자범위는 2만개라서 O(n + m)으로 처리함 여기서 n은 최대 10만, m은 2만
 
 class Solution {
 public:
