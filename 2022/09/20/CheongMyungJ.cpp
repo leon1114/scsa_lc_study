dd235512b76126f98bfb31499c8fb5aada4ccdc4
CheongMyungJ <101340453+CheongMyungJ@users.noreply.github.com>
2022-09-21 01:27:58 +0900

    Update CheongMyungJ.cpp

--- 2022/09/20/CheongMyungJ.cpp
+++ 2022/09/20/CheongMyungJ.cpp
@@ -1,11 +1,10 @@
-//Runtime: 33 ms, faster than 17.98% of C++ online submissions for Basic Calculator II.
-//Memory Usage: 9.5 MB, less than 11.56% of C++ online submissions for Basic Calculator II.
+//Runtime: 8 ms, faster than 89.69% of C++ online submissions for Basic Calculator II.
+//Memory Usage: 8.6 MB, less than 48.31% of C++ online submissions for Basic Calculator II.
 //후위연산자 방식 stack 사용. 곱셈 나눗셈은 바로바로 처리하게 
-
 class Solution {
 public:
 
-	void findNumber(string& s, int& i, stack<int>& num, bool is_minus = false) {
+	int findNumber(string& s, int& i, bool is_minus = false) {
 		while (s[i] == ' ') { i++; }
 		int temp = s[i] - '0';
 		for (int j = i + 1; j < s.length(); j++) {
@@ -15,57 +14,40 @@ public:
 			temp += s[j] - '0';
 			i = j;
 		}
-		num.push(is_minus? -temp : temp);
+		return is_minus ? -temp : temp;
 	}
 
 	int calculate(string s) {
 		stack<int> num;
-		stack<char> oper;
 		for (int i = 0; i < s.length(); i++) {
 			if (s[i] == ' ') continue;
 			if (s[i] >= '0' && s[i] <= '9') {
-				findNumber(s, i, num);
+				num.push(findNumber(s, i));
 			}
-			else if (s[i] == '+') {
-				oper.push(s[i]);
+			else if (s[i] == '-') {
+				i++;
+				num.push(findNumber(s, i, true));
 			}
-            else if (s[i] == '-') {
-                oper.push('+');
-                i++;
-                findNumber(s, i, num, true);
-            }
 			else if (s[i] == '*') {
 				i++;
-				findNumber(s, i, num);
-				int right = num.top();
-				num.pop();
+				int right = findNumber(s, i);
 				int left = num.top();
 				num.pop();
 				num.push(left * right);
 			}
 			else if (s[i] == '/') {
 				i++;
-				findNumber(s, i, num);
-				int right = num.top();
-				num.pop();
+				int right = findNumber(s, i);
 				int left = num.top();
 				num.pop();
 				num.push(left / right);
 			}
 		}
-		while (oper.empty() == false) {
-			int right = num.top();
+		int sum = 0;
+		while (num.empty() == false) {
+			sum += num.top();
 			num.pop();
-			int left = num.top();
-			num.pop();
-			if (oper.top() == '+') {
-				num.push(left + right);
-			}
-			else if (oper.top() == '-') {
-				num.push(left - right);
-			}
-			oper.pop();
 		}
-		return num.top();
+		return sum;
 	}
 };
